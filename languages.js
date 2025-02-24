d1587b634ae09b9c9bed65cf3bdd9218cbcb8fd8
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-02-24 22:14:18 +0000

    update languages

--- languages.js
+++ languages.js
@@ -1,7 +1,36 @@
 var languages = [
     { "language": "Afrikaans", "country": "ZA", "code": "af" },
     { "language": "አማርኛ", "country": "ET", "code": "am" },
-    { "language": "العربية", "country": "SA", "code": "ar" },
+
+    { "language": "العربية", "code": "ar" },
+    { "country": "001", "code": "ar", "map": "ar" },
+    { "country": "AE", "code": "ar", "map": "ar" },
+    { "country": "BH", "code": "ar", "map": "ar" },
+    { "country": "DJ", "code": "ar", "map": "ar" },
+    { "country": "DZ", "code": "ar", "map": "ar" },
+    { "country": "EG", "code": "ar", "map": "ar" },
+    { "country": "ER", "code": "ar", "map": "ar" },
+    { "country": "IL", "code": "ar", "map": "ar" },
+    { "country": "IQ", "code": "ar", "map": "ar" },
+    { "country": "JO", "code": "ar", "map": "ar" },
+    { "country": "KM", "code": "ar", "map": "ar" },
+    { "country": "KW", "code": "ar", "map": "ar" },
+    { "country": "LE", "code": "ar", "map": "ar" },
+    { "country": "LI", "code": "ar", "map": "ar" },
+    { "country": "MA", "code": "ar", "map": "ar" },
+    { "country": "MR", "code": "ar", "map": "ar" },
+    { "country": "OM", "code": "ar", "map": "ar" },
+    { "country": "PS", "code": "ar", "map": "ar" },
+    { "country": "QA", "code": "ar", "map": "ar" },
+    { "country": "SA", "code": "ar", "map": "ar" },
+    { "country": "SD", "code": "ar", "map": "ar" },
+    { "country": "SO", "code": "ar", "map": "ar" },
+    { "country": "SS", "code": "ar", "map": "ar" },
+    { "country": "SY", "code": "ar", "map": "ar" },
+    { "country": "TD", "code": "ar", "map": "ar" },
+    { "country": "TN", "code": "ar", "map": "ar" },
+    { "country": "YE", "code": "ar", "map": "ar" },
+
     { "language": "Mapudungun", "country": "CL", "code": "arn" },
     { "language": "الدارجة المغربية", "country": "MA", "code": "ary" },
     { "language": "অসমীয়া", "country": "IN", "code": "as" },
@@ -9,63 +38,85 @@ var languages = [
     { "language": "Башҡорт", "country": "RU", "code": "ba" },
     { "language": "беларуская", "country": "BY", "code": "be" },
     { "language": "български", "country": "BG", "code": "bg" },
-    { "language": "বাংলা", "country": "BD", "code": "bn" },
+
+    { "language": "বাংলা", "code": "bn"},
+    { "country": "BD", "code": "bn", "map": "bn" },
+    { "country": "IN", "code": "bn", "map": "bn" },
+
     { "language": "བོད་ཡིག", "country": "CN", "code": "bo" },
-    { "language": "brezhoneg", "country": "FR", "code": "br" },
-    { "language": "bosanski/босански", "country": "BA", "code": "bs" },
-    { "language": "català", "country": "ES", "code": "ca" },
+    { "language": "Brezhoneg", "country": "FR", "code": "br" },
+    { "language": "Bosanski", "country": "BA", "code": "bs" },
+    { "language": "Català", "country": "ES", "code": "ca" },
     { "language": "کوردیی ناوەندی", "country": "IQ", "code": "ckb" },
     { "language": "Corsu", "country": "FR", "code": "co" },
-    { "language": "čeština", "country": "CZ", "code": "cs" },
+    { "language": "Čeština", "country": "CZ", "code": "cs" },
     { "language": "Cymraeg", "country": "GB", "code": "cy" },
-    { "language": "dansk", "country": "DK", "code": "da" },
+    { "language": "Dansk", "country": "DK", "code": "da" },
     { "language": "Deutsch", "country": "DE", "code": "de" },
-    { "language": "dolnoserbšćina", "country": "DE", "code": "dsb" },
+    { "language": "Dolnoserbšćina", "country": "DE", "code": "dsb" },
     { "language": "ދިވެހިބަސް", "country": "MV", "code": "dv" },
     { "language": "Ελληνικά", "country": "GR", "code": "el" },
-    { "language": "English", "country": "US", "code": "en" },
-    { "language": "español", "country": "ES", "code": "es" },
-    { "language": "eesti", "country": "EE", "code": "et" },
-    { "language": "euskara", "country": "ES", "code": "eu" },
+
+    { "language": "English", "code": "en", "map": "en"},
+    { "country": "US", "code": "en", "map": "en"},
+    { "country": "CA", "code": "en", "map": "en"},
+    { "country": "GB", "code": "en", "map": "en"},
+    { "country": "AU", "code": "en", "map": "en"},
+    { "country": "NZ", "code": "en", "map": "en"},
+    { "country": "ZA", "code": "en", "map": "en"},
+    { "country": "IN", "code": "en", "map": "en"},
+
+    { "language": "Español", "code": "es", "map": "es" },
+    { "country": "ES", "code": "es", "map": "es" },
+    { "country": "MX", "code": "es", "map": "es" },
+    { "country": "419", "code": "es", "map": "es" },
+
+    { "language": "Eesti", "country": "EE", "code": "et" },
+    { "language": "Euskara", "country": "ES", "code": "eu" },
     { "language": "فارسى", "country": "IR", "code": "fa" },
-    { "language": "suomi", "country": "FI", "code": "fi" },
+    { "language": "Suomi", "country": "FI", "code": "fi" },
     { "language": "Filipino", "country": "PH", "code": "fil" },
-    { "language": "føroyskt", "country": "FO", "code": "fo" },
-    { "language": "français", "country": "FR", "code": "fr" },
+    { "language": "Føroyskt", "country": "FO", "code": "fo" },
+    { "language": "Français", "country": "FR", "code": "fr" },
     { "language": "Frysk", "country": "NL", "code": "fy" },
     { "language": "Gaeilge", "country": "IE", "code": "ga" },
     { "language": "Gàidhlig", "country": "GB", "code": "gd" },
     { "language": "Taetae ni Kiribati", "country": "KI", "code": "gil" },
-    { "language": "galego", "country": "ES", "code": "gl" },
+    { "language": "Galego", "country": "ES", "code": "gl" },
     { "language": "Schweizerdeutsch", "country": "CH", "code": "gsw" },
     { "language": "ગુજરાતી", "country": "IN", "code": "gu" },
     { "language": "Hausa", "country": "NG", "code": "ha" },
     { "language": "עברית", "country": "IL", "code": "he" },
     { "language": "हिंदी", "country": "IN", "code": "hi" },
-    { "language": "hrvatski", "country": "HR", "code": "hr" },
+    { "language": "Hrvatski", "country": "HR", "code": "hr" },
     { "language": "hornjoserbšćina", "country": "DE", "code": "hsb" },
     { "language": "magyar", "country": "HU", "code": "hu" },
     { "language": "Հայերեն", "country": "AM", "code": "hy" },
     { "language": "Bahasa Indonesia", "country": "ID", "code": "id" },
     { "language": "Igbo", "country": "NG", "code": "ig" },
     { "language": "ꆈꌠꁱꂷ", "country": "CN", "code": "ii" },
-    { "language": "íslenska", "country": "IS", "code": "is" },
-    { "language": "italiano", "country": "IT", "code": "it" },
+    { "language": "Íslenska", "country": "IS", "code": "is" },
+    { "language": "Italiano", "country": "IT", "code": "it" },
     { "language": "Inuktitut/ᐃᓄᒃᑎᑐᑦ (ᑲᓇᑕ)", "country": "CA", "code": "iu" },
     { "language": "日本語", "country": "JP", "code": "ja" },
     { "language": "ქართული", "country": "GE", "code": "ka" },
     { "language": "Қазақша", "country": "KZ", "code": "kk" },
-    { "language": "kalaallisut", "country": "GL", "code": "kl" },
+    { "language": "Kalaallisut", "country": "GL", "code": "kl" },
     { "language": "ខ្មែរ", "country": "KH", "code": "km" },
     { "language": "ಕನ್ನಡ", "country": "IN", "code": "kn" },
     { "language": "한국어", "country": "KR", "code": "ko" },
     { "language": "कोंकणी", "country": "IN", "code": "kok" },
-    { "language": "Kurdî/کوردی", "country": "TR", "code": "ku" },
+
+    { "language": "کوردی", "code": "ku" },
+    { "country": "TR", "code": "ku" },
+    { "country": "TR", "code": "kmr", "map": "ku" },
+    { "country": "TR", "code": "sth", "map": "ku" },
+
     { "language": "Кыргыз", "country": "KG", "code": "ky" },
     { "language": "Lëtzebuergesch", "country": "LU", "code": "lb" },
     { "language": "ລາວ", "country": "LA", "code": "lo" },
-    { "language": "lietuvių", "country": "LT", "code": "lt" },
-    { "language": "latviešu", "country": "LV", "code": "lv" },
+    { "language": "Lietuvių", "country": "LT", "code": "lt" },
+    { "language": "Latviešu", "country": "LV", "code": "lv" },
     { "language": "Reo Māori", "country": "NZ", "code": "mi" },
     { "language": "македонски јазик", "country": "MK", "code": "mk" },
     { "language": "മലയാളം", "country": "IN", "code": "ml" },
@@ -75,67 +126,89 @@ var languages = [
     { "language": "Bahasa Malaysia", "country": "MY", "code": "ms" },
     { "language": "Malti", "country": "MT", "code": "mt" },
     { "language": "မြန်မာဘာသာ", "country": "MM", "code": "my" },
-    { "language": "norsk (bokmål)", "country": "NO", "code": "nb" },
+    { "language": "bokmål", "country": "NO", "code": "nb" },
     { "language": "नेपाली (नेपाल)", "country": "NP", "code": "ne" },
     { "language": "Nederlands", "country": "NL", "code": "nl" },
-    { "language": "norsk (nynorsk)", "country": "NO", "code": "nn" },
-    { "language": "norsk", "country": "NO", "code": "no" },
-    { "language": "occitan", "country": "FR", "code": "oc" },
+    { "language": "Nynorsk", "country": "NO", "code": "nn" },
+    { "language": "Norsk", "country": "NO", "code": "no" },
+    { "language": "Occitan", "country": "FR", "code": "oc" },
     { "language": "ଓଡ଼ିଆ", "country": "IN", "code": "or" },
     { "language": "Papiamentu", "country": "AW", "code": "pap" },
     { "language": "ਪੰਜਾਬੀ / پنجابی", "country": "IN", "code": "pa" },
-    { "language": "polski", "country": "PL", "code": "pl" },
+    { "language": "Polski", "country": "PL", "code": "pl" },
     { "language": "درى", "country": "AF", "code": "prs" },
     { "language": "پښتو", "country": "AF", "code": "ps" },
-    { "language": "português", "country": "PT", "code": "pt" },
+
+    { "language": "Português", "code": "pt", "map": "pt" },
+    { "country": "PT", "code": "pt", "map": "pt" },
+    { "country": "BR", "code": "pt", "map": "pt" },
+
     { "language": "K'iche", "country": "GT", "code": "quc" },
-    { "language": "runasimi", "country": "PE", "code": "qu" },
+    { "language": "Runasimi", "country": "PE", "code": "qu" },
     { "language": "Rumantsch", "country": "CH", "code": "rm" },
-    { "language": "română", "country": "RO", "code": "ro" },
+    { "language": "Română", "country": "RO", "code": "ro" },
     { "language": "русский", "country": "RU", "code": "ru" },
     { "language": "Kinyarwanda", "country": "RW", "code": "rw" },
     { "language": "संस्कृत", "country": "IN", "code": "sa" },
     { "language": "саха", "country": "RU", "code": "sah" },
     { "language": "davvisámegiella", "country": "NO", "code": "se" },
     { "language": "සිංහල", "country": "LK", "code": "si" },
-    { "language": "slovenčina", "country": "SK", "code": "sk" },
-    { "language": "slovenski", "country": "SI", "code": "sl" },
+    { "language": "Slovenčina", "country": "SK", "code": "sk" },
+    { "language": "Slovenski", "country": "SI", "code": "sl" },
     { "language": "åarjelsaemiengiele", "country": "NO", "code": "sma" },
     { "language": "julevusámegiella", "country": "NO", "code": "smj" },
     { "language": "sämikielâ", "country": "FI", "code": "smn" },
     { "language": "sääʹmǩiõll", "country": "FI", "code": "sms" },
-    { "language": "shqip", "country": "AL", "code": "sq" },
+    { "language": "Shqip", "country": "AL", "code": "sq" },
     { "language": "srpski/српски", "country": "RS", "code": "sr" },
     { "language": "Sesotho", "country": "LS", "code": "st" },
-    { "language": "svenska", "country": "SE", "code": "sv" },
+    { "language": "Svenska", "country": "SE", "code": "sv" },
     { "language": "Kiswahili", "country": "TZ", "code": "sw" },
     { "language": "ܣܘܪܝܝܐ", "country": "SY", "code": "syc" },
-    { "language": "தமிழ்", "country": "IN", "code": "ta" },
+
+    { "language": "தமிழ்", "code": "ta", "map": "ta" },
+    { "country": "IN", "code": "ta", "map": "ta" },
+    { "country": "LK", "code": "ta", "map": "ta" },
+
     { "language": "తెలుగు", "country": "IN", "code": "te" },
     { "language": "Тоҷикӣ", "country": "TJ", "code": "tg" },
     { "language": "ไทย", "country": "TH", "code": "th" },
-    { "language": "türkmençe", "country": "TM", "code": "tk" },
+    { "language": "Türkmençe", "country": "TM", "code": "tk" },
     { "language": "Setswana", "country": "BW", "code": "tn" },
     { "language": "Türkçe", "country": "TR", "code": "tr" },
     { "language": "Татарча", "country": "RU", "code": "tt" },
     { "language": "Tamazight", "country": "MA", "code": "tzm" },
     { "language": "ئۇيغۇرچە", "country": "CN", "code": "ug" },
     { "language": "українська", "country": "UA", "code": "uk" },
-    { "language": "اُردو", "country": "PK", "code": "ur" },
+
+    { "language": "اُردو", "code": "ur" },
+    { "country": "PK", "code": "ur", "map": "ur" },
+    { "country": "IN", "code": "ur", "map": "ur" },
+
     { "language": "Uzbek/Ўзбек", "country": "UZ", "code": "uz" },
     { "language": "Tiếng Việt", "country": "VN", "code": "vi" },
     { "language": "Wolof", "country": "SN", "code": "wo" },
     { "language": "isiXhosa", "country": "ZA", "code": "xh" },
     { "language": "Yoruba", "country": "NG", "code": "yo" },
-    { "language": "中文", "country": "CN", "code": "zh" },
+
+    { "language": "中文", "code": "zh" },
+    { "country": "CN", "code": "zh", "map": "zh" },
+    { "country": "TW", "code": "zh", "map": "zh" },
+    { "country": "HK", "code": "zh", "map": "zh" },
+    { "country": "HK", "code": "yue", "map": "zh" },
+
     { "language": "isiZulu", "country": "ZA", "code": "zu" }
-]
+];
 
 var langElement = !!langElement ? langElement : document.getElementById('selLang')
 
 langElement.querySelectorAll('option').forEach(o => o.remove())
 
 languages.forEach(l => {
+    addLanguage(l)
+})
+
+function addLanguage(l) {
     var o = document.createElement('option')
 
     o.textContent = `${l.language} (${l.country})`
@@ -145,4 +218,33 @@ languages.forEach(l => {
     o.value = oValue
 
     langElement.appendChild(o)
-})
+}
+
+function getLanguage() {
+    var keys = ['languages', 'language', 'browserLanguage', 'systemLanguage', 'userLanguage']
+    var language = null;
+
+    var isGood = false
+    keys.forEach(k => {
+        var l = navigator[k];
+        if (l && Array.isArray(l) && !isGood) {
+            l.forEach(l => {
+                if (!isGood) {
+                    language = l;
+                    isGood = true
+                    return
+                }
+            })
+            return
+        }
+        else if (l  && !isGood) {
+            language = l;
+            isGood = true
+            return
+        }
+    })
+
+    return language;
+  };
+
+alert(getLanguage());
