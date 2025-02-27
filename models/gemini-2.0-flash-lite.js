366ad71dd944c3a7610fc6bd97e293d3c0f43edf
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-02-26 20:04:14 -0800

    Create gemini-2.0-flash-lite.js

---
+++ models/gemini-2.0-flash-lite.js
@@ -0,0 +1,68 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import {
+    FileState,
+    GoogleAICacheManager,
+    GoogleAIFileManager,
+} from '@google/generative-ai/server';
+
+import config from '../config.js'
+
+const FileObj = (...types) => ({ type: 'file', types });
+
+var systemPrompt = config.systemPrompt
+
+var modelConfig = {
+    provider: 'Google',
+    model: 'gemini-2.0-flash-lite',
+    types: ['text'/*, FileObj('image', 'audio')*/],
+    modelPrompt: 'Unfortunately, you are not yet able to keep context.',
+}
+modelConfig.api_key = process.env[`${modelConfig.provider.toUpperCase()}_API_KEY`]
+
+
+const genAI = new GoogleGenerativeAI(modelConfig.api_key);
+const aiModel = genAI.getGenerativeModel({ model: modelConfig.model });
+
+const chat = aiModel.startChat();
+
+async function newMessage(threadId, prompt, model, type, urls, useSystem=true, startingMessage) {
+    return newCompletion(threadId, prompt, model, type, urls, useSystem=true, startingMessage)
+}
+
+async function newCompletion(threadId, prompt, model, type, urls, useSystem=true, startingMessage) {
+    var messageObj = [prompt]
+
+    if (urls) {
+        urls.forEach(async url => {
+            console.log(url)
+
+            // const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, {
+            //     mimeType: "text/plain",
+            // });
+
+            // messageObj.push(
+            //     {
+            //         fileData: {
+            //             fileUri: uploadResult.file.uri,
+            //             mimeType: uploadResult.file.mimeType,
+            //         },
+            //     },
+            // )
+        })
+    }
+
+    let result = await chat.sendMessage(messageObj);
+
+    return result.response.text()
+}
+
+async function createThread(model) {
+    let sendSPrompt = await chat.sendMessage(systemPrompt);
+    return false
+}
+
+async function deleteThread(id) {
+    return 'done!'
+}
+
+export default { config: modelConfig, message: newMessage, completion: newCompletion, thread: { create: createThread, delete: deleteThread } }
